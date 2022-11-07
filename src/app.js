65f33b0e0137eb580d2e80491fb0c5aef21e6c5d
abimcintyre <abimcintyre@performanceinpeople.co.uk>
2022-11-07 12:57:34 +0000

    Added time and date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response.data);
   let temperature = document.querySelector("#temp");
@@ -10,6 +25,8 @@ function displayWeather(response) {
   humid.innerHTML = response.data.temperature.humidity;
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=1276dbfdft169fo302ba35426e760566&units=metric`;
