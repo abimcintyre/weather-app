9b1bf0c3c1ade2b16e412324b297848c501e1883
abimcintyre <abimcintyre@performanceinpeople.co.uk>
2022-11-07 12:43:44 +0000

    Added display weather function for weather data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayWeather(response) {
+  console.log(response.data);
+  let temperature = document.querySelector("#temp");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+  let info = document.querySelector("#info");
+  info.innerHTML = response.data.condition.description;
+  let humid = document.querySelector("#humid");
+  humid.innerHTML = response.data.temperature.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=1276dbfdft169fo302ba35426e760566&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
